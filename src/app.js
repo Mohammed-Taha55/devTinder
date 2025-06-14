9d3644a40856e041dbb158c59c7965f0eaa08331
Mohammed-Taha55 <mohammedtaha10683@gmail.com>
2025-06-14 21:12:48 +0530

    error-handling

--- src/app.js
+++ src/app.js
@@ -1,19 +1,22 @@
 const express = require('express')
 const app = express();
-const {adminauth, userauth} = require("./middlewares/auth");
-app.use("/admin",adminauth);
-app.post("/user/login",(req,res)=>{
-res.send("user logged in");
-});
-app.get("/user/data",userauth,(req,res)=>{
-    res.send("user sent data");
-});
-app.get("/admin/getdata",(req,res)=>{
-    res.send("get data");
-});
-app.get("/admin/delete/user",(req,res)=>{
-    res.send("deleted user");
-});
+app.use("/get/data",(req,res)=>{
+    try{
+    throw new Error("errrorrr")
+    res.send("user data")
+    }
+    catch(err){
+        if(err){
+            res.status(501).send("please get lost");
+        }
+    }
+    
+})
+app.use("/",(err,req,res,next)=>{
+    if(err){
+        res.status(500).send("this is error");
+    }
+})
 app.listen(7777,()=>{
     console.log("listening on port 7777");
 });
