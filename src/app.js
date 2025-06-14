fe6f83e9432387be2401430f8f27275d696dab76
Mohammed-Taha55 <mohammedtaha10683@gmail.com>
2025-06-14 16:06:15 +0530

    multiple-route handlers

--- src/app.js
+++ src/app.js
@@ -1,21 +1,21 @@
 const express = require('express')
 const app = express();
-app.get("/user",(req,res)=>{
-res.send({first:"taha",last:"hero"})
-});
-app.use("/test",(req,res)=>{
-    res.send("hello from test!");
-});
-app.post("/user",(req,res)=>{
-    //saving data db logic
-    res.send("data succesfully saved in database")
-})
-app.delete("/user",(req,res)=>{
-    res.send("succesfully deleted")
-})
-app.patch("/user",(req,res)=>{
-    res.send("this is patch")
-})
+app.get(
+    "/user",
+    (req,res,next)=>{
+    console.log("handling the user router 1!!");
+    next();
+// res.send("1st response")
+},
+(req,res,next)=>{
+    console.log("handling the user router 2!!");
+    next()
+},
+(req,res,next)=>{
+console.log("handling router user 3!!");
+res.send("3rd response");
+}
+);
 app.listen(7777,()=>{
     console.log("listening on port 7777");
 });
