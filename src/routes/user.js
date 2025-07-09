bacebd92e6560c679487ee96714245360fae39e9
Mohammed-Taha55 <mohammedtaha10683@gmail.com>
2025-07-09 22:52:59 +0530

    updates-3

--- src/routes/user.js
+++ src/routes/user.js
@@ -28,11 +28,12 @@ userRouter.get("/user/connections", userauth, async (req, res) =>{
 const loggedInUser = req.user;
 const connectionRequests = await ConnectionRequest.find({
     $or: [
-        { toUserId: loggedInUser._id, status: "accepted "},
-        { fromUserId: loggedInUser._id, status: "accepted "},
+        { toUserId: loggedInUser._id, status: "accepted" },
+        { fromUserId: loggedInUser._id, status: "accepted" },
 
-    ]
-}).populate("fromUserId", USER_SAFE_DATA )
+    ],
+})
+  .populate("fromUserId", USER_SAFE_DATA )
   .populate("toUserId", USER_SAFE_DATA);
 const data = connectionRequests.map((row) => {
 if(row.fromUserId._id.toString() == loggedInUser._id.toString()){
